0b0f3bbf295bf348d02d500c11dedd440bbc3335
sospina <sigifredo.ospina@kaplan.edu>
2019-11-20 12:47:07 -0500

    Add project files.

---
+++ src/APAC.Garb.Data/Models/Common/LookUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APAC.Garb.Data.Models.Common
+{
+    public class Lookup
+    {        
+        /// <inheritdocs />
+        public int Id { get; set; }
+        public String Name { get; set;  }
+        public String Code { get; set; }
+    }
+}
---
+++ src/APAC.Garb.Data/Models/Garbs/Garb.cs
@@ -0,0 +1,13 @@
+﻿using APAC.Garb.Data.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APAC.Garb.Data.Models.Garbs
+{
+    public class Garb
+    {        
+        /// <inheritdocs />
+        public List<Lookup> Garbs { get; set; }
+    }
+}
