6018f173ac5712c94c2eeb17b0d0688c90208812
sospina <sigifredo.ospina@kaplan.edu>
2019-12-03 18:06:32 -0500

    added EntityFramework, Mapper, Logger, Controller, Repository, DbContext, DataModels

---
+++ src/APAC.Garb.Data/DbContexts/RequestItemContext.cs
@@ -0,0 +1,35 @@
+﻿using APAC.Garb.Data.Models.Definition.Common;
+using Microsoft.EntityFrameworkCore;
+using APAC.Garb;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APAC.Garb.Data.DbContexts
+{
+    public class RequestItemContext:DbContext
+    {
+        public DbSet<ILookup> RequestItems { get; set; }
+
+        public RequestItemContext(DbContextOptions<RequestItemContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<APAC.Garb.Data.Entities.Garb>().HasData(
+                new APAC.Garb.Data.Entities.Garb
+                { 
+                     Active = true,
+                     Description = "Shirt",
+                     Id = 1,
+                     TypeId = 1,
+                     Name = "Shirt",
+                }
+            );
+           
+        }
+    }
+}
---
+++ src/APAC.Garb.Service/Controllers/RequestItemsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APAC.Garb.Service.Controllers
+{
+    public class RequestItemsController : ControllerBase
+    {
+        private readonly ILogger<RequestItemsController> _logger;
+        private readonly IMapper _mapper;
+        //private readonly 
+        public RequestItemsController(ILogger<RequestItemsController> logger, IMapper mapper)
+        {
+           
+
+        }
+    }
+}
